6e2fcf34972d5015cf94713013edde4cf8abbb43
Chap Ambrose <chapambrose@gmail.com>
2010-03-20 11:12:24 -0400

    added unit test for accepts_nested_attributes_for bug

--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -1,8 +1,32 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "accepts_nested_attributes_for not changing parent record will fail" do
+    projects(:one).update_attributes :name => projects(:one).name,
+                                     :tasks_attributes => [{
+                                       :id => tasks(:one).id,
+                                       :name => tasks(:one).name,
+                                       :assignments_attributes => [{
+                                         :id => assignments(:one).id,
+                                        :name => 'Paul'
+                                       }]
+                                      }]
+    assignments(:one).reload
+    assert_equal 'Paul', assignments(:one).name
   end
+  
+  test "accepts_nested_attributes_for changing parent record will pass" do
+    projects(:one).update_attributes :name => projects(:one).name,
+                                     :tasks_attributes => [{
+                                       :id => tasks(:one).id,
+                                       :name => 'Buy gasoline and oil',
+                                       :assignments_attributes => [{
+                                         :id => assignments(:one).id,
+                                        :name => 'Paul'
+                                       }]
+                                      }]
+    assignments(:one).reload
+    assert_equal 'Paul', assignments(:one).name
+  end
+
 end
